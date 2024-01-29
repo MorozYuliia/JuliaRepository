81196c55cb2a78684d95e76d7cd74865011354e9
yuliia moroz <julliamoroz1997@gmail.com>
2024-01-29 10:44:26 +0200

    add valurable examples

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Numerics;
+
 Console.WriteLine("Hello, 123");
+byte myByteNumber = 30;
+short myShort = 15123;
+int myIntNumber = 7;
+bool myBoolValue = true;
+long myLongNumber = 114545897;
+float myFloat = 125.89f;
+double myDouble = 178.25575d;
+decimal myDecimal = 1457.455898998979m;
+
+
+Console.WriteLine(myByteNumber);
+Console.WriteLine(myShort);
+Console.WriteLine(myIntNumber);
+Console.WriteLine(myBoolValue);
+Console.WriteLine(myLongNumber);
+Console.WriteLine(myFloat);
+Console.WriteLine(myDouble);
+Console.WriteLine(myDecimal);
+
+

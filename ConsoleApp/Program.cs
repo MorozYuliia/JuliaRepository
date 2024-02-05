a84940ae5ff7eb2344bdb6b06123d7b749de7640
yuliia moroz <julliamoroz1997@gmail.com>
2024-02-05 12:44:59 +0200

    Fibanachoo numbers

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello World");
+﻿const int N = 8;
+var result = 1;
+int firstValue = 1;
+int secondValue = 1;
+
+for (int i = 2; i <=N-1; ++i) {
+    result = firstValue + secondValue;
+    firstValue = secondValue;
+    secondValue = result;
+    
+};
+Console.WriteLine($"{N}-th Фібаначчі число це: {result}");

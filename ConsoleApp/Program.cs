d39402856cdbcd8fd973ac6b03566da3df41d273
yuliia moroz <julliamoroz1997@gmail.com>
2024-02-05 10:08:12 +0200

    Home work - Conditional Operators

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello World!")
+﻿int x = 3;
+int y = 3;
+int sum = 0;
+if (x < y) {
+    for (int i = x; i<=y; i++) {
+        sum +=i;
+    }
+    Console.WriteLine($"Сума чисел: {sum}");
+}
+else if (x >y) {
+    for (int i = y; i<=x; i++) {
+        sum +=i;
+    }
+     Console.WriteLine($"Сума чисел: {sum}");
+}
+else if (x == y) {
+    sum = x;
+    Console.WriteLine($"Сума чисел: {sum}");
+}

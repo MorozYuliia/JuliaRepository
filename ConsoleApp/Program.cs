f3de66f0290ea7416861fd61f965a89a5a1207d4
yuliia moroz <julliamoroz1997@gmail.com>
2024-02-26 15:03:23 +0200

    first commit

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,33 +1,4 @@
-﻿// Write to Console number from 1 to n;
-int num = 5;
-int start = 1;
-Rec(num, start);
-Writeline(6);
-// First option
-static void Writeline (int n) {
-   for (int i =1; i <= n; i++)
-   Console.WriteLine(i);
-};
-
-// Second option
-static void Rec(int num, int start)
-{
-   if (num >= start)
-   {
-      Console.WriteLine(start);
-      Rec(num, start +1);
-   }
-
-};
-
-// Create Method Factorial
-Console.WriteLine(Factorial(5));
-
-int Factorial (int n) {
-if (n==1) return n;
-return n*Factorial(n-1);
-};
-
+﻿// Insertion Sort;
 
 
  
